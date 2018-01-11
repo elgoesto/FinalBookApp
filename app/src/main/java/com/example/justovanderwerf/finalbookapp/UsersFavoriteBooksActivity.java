f5205e88182985041e2d8965461fc200890d972f
Justo van der Werf <justovdwerf@gmail.com>
2018-01-11 15:36:48 +0100

    werkende xmls en knoppen

--- app/src/main/java/com/example/justovanderwerf/finalbookapp/UsersFavoriteBooksActivity.java
+++ app/src/main/java/com/example/justovanderwerf/finalbookapp/UsersFavoriteBooksActivity.java
@@ -1,7 +1,9 @@
 package com.example.justovanderwerf.finalbookapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UsersFavoriteBooksActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class UsersFavoriteBooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_favorite_books);
+    }
+
+    public void homeOnClick(View view) {
+        Intent intent = new Intent(this, BookOverviewAcivity.class);
+        startActivity(intent);
+    }
+
+    public void overviewOnClick(View view) {
+        Intent intent = new Intent(this, UsersFavoriteBooksActivity.class);
+        startActivity(intent);
+    }
+
+    public void friendsOnClick(View view) {
+        Intent intent = new Intent(this, FriendsOverviewActivity.class);
+        startActivity(intent);
     }
 }
